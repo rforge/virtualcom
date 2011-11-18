215dbcc608a000db72a14717372006bdc559b962
gallien <gallien@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-18 11:30:52 +0000

    minor changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/virtualcom@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/div.param.native.obs.R
+++ pkg/R/div.param.native.obs.R
@@ -5,31 +5,37 @@ div.param.native.obs <- function(spSite, phy, fun,niche.optima,tree){
     
     spSite.rao<-as.data.frame(t(spSite))
     spSite.rao.dummy<-replace(spSite.rao,spSite.rao>0,1)
-    return(list(PD_pa_mpd = mpd(spSite, phy, abundance.weighted=FALSE),
-    PD_pa_mntd = mntd(spSite, phy, abundance.weighted=FALSE),
-    PD_pa_mntd.med = mntd_median(spSite, phy),
-    FD_pa_mpd= mpd(spSite, fun, abundance.weighted=FALSE),
-    FD_pa_mntd= mntd(spSite, fun, abundance.weighted=FALSE),
-    FD_pa_mntd = mntd_median(spSite, fun),
-    PD_ab_mpd = mpd(spSite, phy, abundance.weighted=TRUE),
-    PD_ab_mntd = mntd(spSite, phy, abundance.weighted=TRUE),
-    PD_ab_rao.a = t(divc(spSite.rao, as.dist(phy))),
-    PD_pa_rao.a = t(divc(spSite.rao.dummy, as.dist(phy))),
-    FD_ab_mpd = mpd(spSite, fun, abundance.weighted=TRUE),
-    FD_ab_mntd = mntd(spSite, fun, abundance.weighted=TRUE),
-    FD_ab_rao.a = t(divc(spSite.rao, as.dist(fun))),
-    FD_pa_rao.a = t(divc(spSite.rao.dummy, as.dist(fun))),
-    TD_ab_simpson = diversity(spSite,index="simpson"),
-    TD_ab_shannon = diversity(spSite,index="shannon"),
-    TD_pa_simpson = diversity(spSite.dummy,index="simpson"),
-    TD_pa_shannon = diversity(spSite.dummy,index="shannon"),
-    FD_ab_CWM = apply(spSite,1,function(x){weighted.mean(niche.optima,w=x)}),
-    FD_pa_CWM = apply(spSite.dummy,1,function(x){weighted.mean(niche.optima,w=x)}),
-    FD_ab_CSD = apply(spSite,1,function(x){sqrt(wtd.var(niche.optima,weights=x))}),
-    FD_pa_CSD = apply(spSite.dummy,1,function(x){sqrt(wtd.var(niche.optima,weights=x))}),
-    PD_pa_faith = apply(spSite,1,function(x){
-      tree.red<-drop.tip(tree,as.character(tree$tip.label[x==0]))
-      y<-sum(tree.red$edge.length)
-      return(y)})
-    ))
+    return(list(
+				TD_pa_simpson = diversity(spSite.dummy,index="simpson"),
+				TD_pa_shannon = diversity(spSite.dummy,index="shannon"),
+				TD_ab_simpson = diversity(spSite,index="simpson"),
+				TD_ab_shannon = diversity(spSite,index="shannon"),
+				
+				FD_pa_mpd= mpd(spSite, fun, abundance.weighted=FALSE),
+				FD_pa_mntd= mntd(spSite, fun, abundance.weighted=FALSE),
+#FD_pa_mntd.med = mntd_median(spSite, fun),
+				FD_pa_rao.a = t(divc(spSite.rao.dummy, as.dist(fun))),
+				FD_pa_CWM = apply(spSite.dummy,1,function(x){weighted.mean(niche.optima,w=x)}),
+				FD_pa_CSD = apply(spSite.dummy,1,function(x){sqrt(wtd.var(niche.optima,weights=x))}),
+				
+				FD_ab_mpd = mpd(spSite, fun, abundance.weighted=TRUE),
+				FD_ab_mntd = mntd(spSite, fun, abundance.weighted=TRUE),
+				FD_ab_rao.a = t(divc(spSite.rao, as.dist(fun))),
+				FD_ab_CWM = apply(spSite,1,function(x){weighted.mean(niche.optima,w=x)}),
+				FD_ab_CSD = apply(spSite,1,function(x){sqrt(wtd.var(niche.optima,weights=x))}),
+   		    	
+				PD_pa_mpd = mpd(spSite, phy, abundance.weighted=FALSE),
+				PD_pa_mntd = mntd(spSite, phy, abundance.weighted=FALSE),
+#PD_pa_mntd.med = mntd_median(spSite, phy),
+				PD_pa_rao.a = t(divc(spSite.rao.dummy, as.dist(phy))),
+				PD_pa_faith = apply(spSite,1,function(x){
+									tree.red<-drop.tip(tree,as.character(tree$tip.label[x==0]))
+									y<-sum(tree.red$edge.length)
+									return(y)}),
+				
+				PD_ab_mpd = mpd(spSite, phy, abundance.weighted=TRUE),
+				PD_ab_mntd = mntd(spSite, phy, abundance.weighted=TRUE),
+				PD_ab_rao.a = t(divc(spSite.rao, as.dist(phy)))
+				
+				))
 }
