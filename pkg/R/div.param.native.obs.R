801cb9b9bce90e32f60a02b156be04aee440eaae
tami <tami@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-24 14:45:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/virtualcom@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/div.param.native.obs.R
+++ pkg/R/div.param.native.obs.R
@@ -22,51 +22,51 @@ div.param.native.obs <- function(spSite, phy, fun, niche.optima, tree, indX.nat)
       PD_ab_vill = dbFD(phy.NoZero, spSite.NoZero, w.abun = FALSE, calc.FRic = FALSE, calc.CWM = FALSE, calc.FDiv = FALSE, messages = FALSE)
     }
     
-    results <- list(
-				if("TD_pa_simpson" %in% indX.nat) {TD_pa_simpson = diversity(spSite.dummy, index="simpson")},
-				if("TD_pa_shannon" %in% indX.nat) {TD_pa_shannon = diversity(spSite.dummy, index="shannon")},
-				if("TD_ab_simpson" %in% indX.nat) {TD_ab_simpson = diversity(spSite, index="simpson")},
-				if("TD_ab_shannon" %in% indX.nat) {TD_ab_shannon = diversity(spSite, index="shannon")},
-				
-				if("FD_pa_mpd" %in% indX.nat) {FD_pa_mpd= mpd(spSite, fun, abundance.weighted=FALSE)},
-				if("FD_pa_mntd" %in% indX.nat) {FD_pa_mntd= mntd(spSite, fun, abundance.weighted=FALSE)},
-        if("FD_pa_CWM" %in% indX.nat) {FD_pa_CWM = apply(spSite.dummy, 1, function(x){weighted.mean(niche.optima, w=x)})},
-				if("FD_pa_CSD" %in% indX.nat) {FD_pa_CSD = apply(spSite.dummy, 1, function(x){sqrt(wtd.var(niche.optima, weights=x))})},
-        if("FD_pa_FEve" %in% indX.nat) {FD_pa_FEve = FD_pa_vill$FEve},
-        if("FD_pa_FDis" %in% indX.nat) {FD_pa_FDis = FD_pa_vill$FDis},
-        if("FD_pa_faith" %in% indX.nat) {FD_pa_faith = treedive(spSite, hclust(as.dist(fun)))},				
-				
-				if("FD_ab_mpd" %in% indX.nat) {FD_ab_mpd = mpd(spSite, fun, abundance.weighted=TRUE)},
-				if("FD_ab_mntd" %in% indX.nat) {FD_ab_mntd = mntd(spSite, fun, abundance.weighted=TRUE)},
-				if("FD_ab_CWM" %in% indX.nat) {FD_ab_CWM = apply(spSite, 1, function(x){weighted.mean(niche.optima,w=x)})},
-				if("FD_ab_CSD" %in% indX.nat) {FD_ab_CSD = apply(spSite, 1, function(x){sqrt(wtd.var(niche.optima,weights=x))})},
-				if("FD_ab_FEve" %in% indX.nat) {FD_ab_FEve = FD_ab_vill$FEve},
-        if("FD_ab_FDis" %in% indX.nat) {FD_ab_FDis = FD_ab_vill$FDis},
+    results <- list()
+		if("TD_pa_simpson" %in% indX.nat) {results$TD_pa_simpson = diversity(spSite.dummy, index="simpson")}
+		if("TD_pa_shannon" %in% indX.nat) {results$TD_pa_shannon = diversity(spSite.dummy, index="shannon")}
+		if("TD_ab_simpson" %in% indX.nat) {results$TD_ab_simpson = diversity(spSite, index="simpson")}
+		if("TD_ab_shannon" %in% indX.nat) {results$TD_ab_shannon = diversity(spSite, index="shannon")}
+		
+		if("FD_pa_mpd" %in% indX.nat) {results$FD_pa_mpd= mpd(spSite, fun, abundance.weighted=FALSE)}
+		if("FD_pa_mntd" %in% indX.nat) {results$FD_pa_mntd= mntd(spSite, fun, abundance.weighted=FALSE)}
+    if("FD_pa_CWM" %in% indX.nat) {results$FD_pa_CWM = apply(spSite.dummy, 1, function(x){weighted.mean(niche.optima, w=x)})}
+		if("FD_pa_CSD" %in% indX.nat) {results$FD_pa_CSD = apply(spSite.dummy, 1, function(x){sqrt(wtd.var(niche.optima, weights=x))})}
+    if("FD_pa_FEve" %in% indX.nat) {results$FD_pa_FEve = FD_pa_vill$FEve}
+    if("FD_pa_FDis" %in% indX.nat) {results$FD_pa_FDis = FD_pa_vill$FDis}
+    if("FD_pa_faith" %in% indX.nat) {results$FD_pa_faith = treedive(spSite, hclust(as.dist(fun)))}				
+		
+		if("FD_ab_mpd" %in% indX.nat) {results$FD_ab_mpd = mpd(spSite, fun, abundance.weighted=TRUE)}
+		if("FD_ab_mntd" %in% indX.nat) {results$FD_ab_mntd = mntd(spSite, fun, abundance.weighted=TRUE)}
+		if("FD_ab_CWM" %in% indX.nat) {results$FD_ab_CWM = apply(spSite, 1, function(x){weighted.mean(niche.optima,w=x)})}
+		if("FD_ab_CSD" %in% indX.nat) {results$FD_ab_CSD = apply(spSite, 1, function(x){sqrt(wtd.var(niche.optima,weights=x))})}
+		if("FD_ab_FEve" %in% indX.nat) {results$FD_ab_FEve = FD_ab_vill$FEve}
+    if("FD_ab_FDis" %in% indX.nat) {results$FD_ab_FDis = FD_ab_vill$FDis}
 
-   		    	
-				if("PD_pa_mpd" %in% indX.nat) {PD_pa_mpd = mpd(spSite, phy, abundance.weighted=FALSE)},
-				if("PD_pa_mntd" %in% indX.nat) {PD_pa_mntd = mntd(spSite, phy, abundance.weighted=FALSE)},
-				if("PD_pa_faith" %in% indX.nat) {PD_pa_faith = apply(spSite, 1, function(x){
-									tree.red <- drop.tip(tree,as.character(tree$tip.label[x==0]))
-									sum(tree.red$edge.length)})},
-				if("PD_pa_colless" %in% indX.nat) {PD_pa_colless = apply(spSite, 1, function(x){
-            tree.red <- drop.tip(tree, as.character(tree$tip.label[x==0]))
-            colless(as.treeshape(tree.red), norm="yule")})},
-				if("PD_pa_betasplit" %in% indX.nat) {PD_pa_betasplit = apply(spSite, 1, function(x){
-            tree.red <- drop.tip(tree, as.character(tree$tip.label[x==0]))
-            maxlik.betasplit(as.treeshape(tree.red))$max_lik})},           
-        if("PD_pa_FEve" %in% indX.nat) {PD_pa_FEve = PD_pa_vill$FEve},
-        if("PD_pa_FDis" %in% indX.nat) {PD_pa_FDis = PD_pa_vill$FDis},
-				
-				if("PD_ab_mpd" %in% indX.nat) {PD_ab_mpd = mpd(spSite, phy, abundance.weighted=TRUE)},
-				if("PD_ab_mntd" %in% indX.nat) {PD_ab_mntd = mntd(spSite, phy, abundance.weighted=TRUE)},
-				if("PD_ab_FEve" %in% indX.nat) {PD_ab_FEve = PD_ab_vill$FEve},
-        if("PD_ab_FDis" %in% indX.nat) {PD_ab_FDis = PD_ab_vill$FDis}
-    )
+	    	
+		if("PD_pa_mpd" %in% indX.nat) {results$PD_pa_mpd = mpd(spSite, phy, abundance.weighted=FALSE)}
+		if("PD_pa_mntd" %in% indX.nat) {results$PD_pa_mntd = mntd(spSite, phy, abundance.weighted=FALSE)}
+		if("PD_pa_faith" %in% indX.nat) {results$PD_pa_faith = apply(spSite, 1, function(x){
+							tree.red <- drop.tip(tree,as.character(tree$tip.label[x==0]))
+							sum(tree.red$edge.length)})}
+		if("PD_pa_colless" %in% indX.nat) {results$PD_pa_colless = apply(spSite, 1, function(x){
+        tree.red <- drop.tip(tree, as.character(tree$tip.label[x==0]))
+        colless(as.treeshape(tree.red), norm="yule")})}
+		if("PD_pa_betasplit" %in% indX.nat) {results$PD_pa_betasplit = apply(spSite, 1, function(x){
+        tree.red <- drop.tip(tree, as.character(tree$tip.label[x==0]))
+        maxlik.betasplit(as.treeshape(tree.red))$max_lik})}           
+    if("PD_pa_FEve" %in% indX.nat) {results$PD_pa_FEve = PD_pa_vill$FEve}
+    if("PD_pa_FDis" %in% indX.nat) {results$PD_pa_FDis = PD_pa_vill$FDis}
+		
+		if("PD_ab_mpd" %in% indX.nat) {results$PD_ab_mpd = mpd(spSite, phy, abundance.weighted=TRUE)}
+		if("PD_ab_mntd" %in% indX.nat) {results$PD_ab_mntd = mntd(spSite, phy, abundance.weighted=TRUE)}
+		if("PD_ab_FEve" %in% indX.nat) {results$PD_ab_FEve = PD_ab_vill$FEve}
+    if("PD_ab_FDis" %in% indX.nat) {results$PD_ab_FDis = PD_ab_vill$FDis}
 
 
-		tt <- sapply(results,function(X){!is.null(X)})
+
+		tt <- sapply(results, function(X){!is.null(X)})
 		results2 <- results[tt]
-		names(results2) <- indX.nat				
+		#names(results2) <- indX.nat				
 		return(results2)
 }
