9a2cbee681f35e87b7c76a6f85ce0049323f04cd
mcarboni <mcarboni@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-15 14:16:26 +0000

    correcting error for flexible moving window
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/virtualcom@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/moving.window.r
+++ pkg/R/moving.window.r
@@ -7,8 +7,9 @@ moving.window<-function(com_land, cell=1, step.length=1){
   
   #landscape dimensions
   side<-length(com_land[com_land$X==1,"X"])
-  new.com <- seq(1, (side - step.length), by=step.length)
+  new.com <- seq(1, (side - (cell- step.length)), by=step.length)
   side2 <- length(new.com)
+  nsp<-ncol(com_land)-3
   
   #aggregated landscape structure
   agg_land <- as.data.frame(matrix(NA,nrow=(side2*side2),ncol=ncol(com_land),dimnames=list(c(1:(side2*side2)),names(com_land))))
@@ -17,7 +18,7 @@ moving.window<-function(com_land, cell=1, step.length=1){
   
   for (i in 1:side2)  {
     for (j in 1:side2)  {
-        agg_land[which(agg_land$X==i & agg_land$Y==j),c(4:103)] <-  colSums(com_land[which(com_land$X %in% c(new.com[i]:(new.com[i]+(cell-1))) & com_land$Y %in% c(new.com[j]:(new.com[j]+(cell-1)))),4:103])
+        agg_land[which(agg_land$X==i & agg_land$Y==j),c(4:(nsp+3))] <-  colSums(com_land[which(com_land$X %in% c(new.com[i]:(new.com[i]+(cell-1))) & com_land$Y %in% c(new.com[j]:(new.com[j]+(cell-1)))),4:(nsp+3)])
         agg_land[which(agg_land$X==i & agg_land$Y==j),"env"] <-  mean(com_land[which(com_land$X %in% c(new.com[i]:(new.com[i]+(cell-1))) & com_land$Y %in% c(new.com[j]:(new.com[j]+(cell-1)))),"env"])
         }
       }
