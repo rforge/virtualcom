ac230108720ad18c5e594f719d2972d6727e701e
tami <tami@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-07-25 09:13:41 +0000

    virtualcom
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/virtualcom@83 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/spsXsites.R
+++
@@ -1,8 +0,0 @@
-spsXsites <- function(i, Nsp, my.matrix, my.list){
-	for(j in 1:Nsp){ 		# To fill the siteXspecies matrix
- 		if(colnames(my.matrix)[j] %in% colnames(my.list[[i]]$spcXs)) {
- 			my.matrix[i,j] <- my.list[[i]]$spcXs[,colnames(my.matrix)[j]]
- 		}                                                                                         
- 	}
- 	return(my.matrix[i,])
-}
